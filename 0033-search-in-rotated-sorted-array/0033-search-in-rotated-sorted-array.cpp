af30bb3975d8623ee82c5591b1948334506db359
gitbotbyte <jagritahuja6@gmail.com>
2026-08-27 22:50:58 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
+    int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int res = -1;
         int low = 0;
         int high = n - 1;
         while (low <= high) {
             int guess = (low + high) / 2;
-            if (nums[guess] > nums[n - 1]) {
-                low = guess + 1;
-            } else {
-                res = guess;
-                high = guess - 1;
+            if (nums[guess] == target) {
+                return guess;
+                break;
+            }
+            if (nums[guess] > nums[n - 1]) { // P1
+                if (nums[guess] < target)
+                    low = guess + 1;
+                else {
+                    if (nums[0] > target)
+                        low = guess + 1;
+                    else
+                        high = guess - 1;
+                }
+                continue;
+            } else { // P2
+                if (nums[guess] > target)
+                    high = guess - 1;
+                else {
+                    if (nums[n - 1] < target)
+                        high = guess - 1;
+                    else
+                        low = guess + 1;
+                }
             }
         }
-        return res;
-    }
-    int binary(vector<int>& nums, int target, int low, int high) {
-        int sol = -1;
-        int n = nums.size();
-        while (low <= high) {
-            int guess = (low + high) / 2;
-            if (nums[guess] == target) {
-                sol = guess;
-                break;
-            } else if (nums[guess] > target)
-                high = guess - 1;
-            else
-                low = guess + 1;
-        }
-        return sol;
-    }
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int minindex = findMin(nums);
-        int firsthalf = binary(nums, target, 0, minindex-1);
-        int sechalf = binary(nums, target, minindex, n - 1);
-        if (firsthalf == -1) {
-            return sechalf;
-        } else
-            return firsthalf;
+        return -1;
     }
 };
