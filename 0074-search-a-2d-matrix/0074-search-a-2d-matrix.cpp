229f8fe9c3bbfb9780a697404fc9262a79b4bdd0
Jagrit <jagritahuja6@gmail.com>
2026-09-05 21:50:03 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.52%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int binary(vector<vector<int>>& a, int target,long long low, long long high){
-        long long row=-1;
-        while(low<=high){
-            long long guess=(low+high)/2;
-            if(a[guess][0]<=target){
-                row=guess;
-                low=guess+1;
-            }
-            else{
-                high=guess-1;
-            }
-        }
-        return row;
-    }
     bool searchMatrix(vector<vector<int>>& a, int target) {
-        int n=a.size();
-        int row=binary(a,target,0,n-1);
-        if(row==-1)return false;
-        int n1=a[row].size();
-        long long low=0,high=n1-1;
-        while(low<=high){
-            long long guess=(low+high)/2;
-            if(a[row][guess]==target){
+        int n = a.size();
+        int m = a[0].size();
+        int low = 0;
+        int high = n * m - 1;
+        while (low <= high) {
+            int guess = (low + high) / 2;
+            int row = guess / m;
+            int col = guess % m;
+            if (a[row][col] == target) {
                 return true;
+            } else if (a[row][col] > target) {
+                high = guess - 1;
+            } else {
+                low = guess + 1;
             }
-            else if(a[row][guess]<target)    low=guess+1;
-            else high=guess-1;
         }
         return false;
     }
