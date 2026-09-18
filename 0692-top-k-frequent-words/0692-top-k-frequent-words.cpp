a17c6b381d2e2c09c0100cd77624f14f9ad76574
Jagrit <jagritahuja6@gmail.com>
2026-09-18 15:51:44 +0530

    Time: 0 ms (100%), Space: 18.3 MB (9.95%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,35 +1,38 @@
 struct cmp {
-    bool operator()(pair<int, string>& a, pair<int, string>& b) {
-        if (a.first != b.first)
-            return a.first > b.first;
-        return a.second < b.second;
+    bool operator()(const pair<string, int>& a, const pair<string, int>& b) const {
+        if (a.second != b.second)
+            return a.second > b.second;
+        return a.first < b.first;
     }
 };
+
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& a, int k) {
-        priority_queue<pair<int, string>, vector<pair<int, string>>, cmp> pq;
         unordered_map<string, int> mp;
-        for (int i = 0; i < a.size(); i++)
+        for (int i = 0; i < a.size(); i++) {
             mp[a[i]]++;
-        for (auto i : mp) {
-            int freq = i.second;
-            string word = i.first;
-            pair<int, string> curr = {freq, word};
-            if (pq.size() < k) {
-                pq.push(curr);
-                continue;
-            }
-
-            pq.push(curr);
-            if (pq.size() > k)
-                pq.pop();
         }
+        
+        priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> pq;      
+        
+        for (auto i : mp) {
+            string word = i.first;
+            int freq = i.second;
+            pair<string, int> curr = {word, freq};
+            
+            pq.push(curr);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        
         vector<string> res;
         while (!pq.empty()) {
-            res.push_back(pq.top().second);
+            res.push_back(pq.top().first);
             pq.pop();
         }
+        
         reverse(res.begin(), res.end());
         return res;
     }
