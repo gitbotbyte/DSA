6a2441dd6253f60a0326324c260752aec1962533
Jagrit <jagritahuja6@gmail.com>
2026-09-18 16:22:39 +0530

    Time: 0 ms (100%), Space: 18 MB (53.26%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,6 +1,6 @@
 struct cmp {
     bool operator()(pair<int, int>& a, pair<int, int>& b) {
-        if (a.first != b.first)
+        if (a.first != b.first) 
             return a.first > b.first;
         return a.second > b.second;
     }
@@ -16,14 +16,9 @@ public:
             int element = i.first;
             int freq = i.second;
             pair<int, int> curr = {freq, element};
-            if (pq.size() < k) {
-                pq.push(curr);
-                continue;
-            }
-            if (curr.first < pq.top().first)
-                continue;
-            pq.pop();
             pq.push(curr);
+            if(pq.size()>k)
+            pq.pop();
         }
         vector<int> res;
         while (!pq.empty()) {
