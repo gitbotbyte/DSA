9881a9d9760ab030e25a653110e8128b4f3172dd
Jagrit <jagritahuja6@gmail.com>
2026-09-19 13:08:20 +0530

    Time: 59 ms (46.78%), Space: 72.7 MB (51.77%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int fun(vector<int> i){
+        int x=i[0];
+        int y=i[1];
+        int dist=(x*x)+(y*y);
+        return dist;
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& a, int k) {
+        priority_queue<pair<int, int>>pq;
+        for(int i=0;i<a.size();i++){
+            int dist=fun(a[i]);
+            int idx=i;
+            pair<int,int > curr={dist,idx};
+            pq.push(curr);
+            if(pq.size()>k) pq.pop();
+        }
+        vector<vector<int>> res;
+        while(!pq.empty()){
+            int index=pq.top().second;
+            res.push_back(a[index]);
+            pq.pop();
+        }
+        return res;
+    }
+};
