a5f6362e57be13c4846485fa90b95398e4da5450
Jagrit <jagritahuja6@gmail.com>
2026-09-19 13:14:43 +0530

    Time: 27 ms (85.64%), Space: 67 MB (78.75%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int fun(vector<int> i){
-        int x=i[0];
-        int y=i[1];
-        int dist=(x*x)+(y*y);
+    int fun(vector<int>& i) {
+        int x = i[0];
+        int y = i[1];
+        int dist = (x * x) + (y * y);
         return dist;
     }
     vector<vector<int>> kClosest(vector<vector<int>>& a, int k) {
-        priority_queue<pair<int, int>>pq;
-        for(int i=0;i<a.size();i++){
-            int dist=fun(a[i]);
-            int idx=i;
-            pair<int,int > curr={dist,idx};
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(NULL);
+        priority_queue<pair<int, int>> pq;
+        for (int i = 0; i < a.size(); i++) {
+            int dist = fun(a[i]);
+            int idx = i;
+            pair<int, int> curr = {dist, idx};
             pq.push(curr);
-            if(pq.size()>k) pq.pop();
+            if (pq.size() > k)
+                pq.pop();
         }
         vector<vector<int>> res;
-        while(!pq.empty()){
-            int index=pq.top().second;
+        while (!pq.empty()) {
+            int index = pq.top().second;
             res.push_back(a[index]);
             pq.pop();
         }
